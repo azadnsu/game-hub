833e8536c0b6d2536fe7334ec27262a8a079430d
Azad Husen <azad.husen@visioncraft.ee>
2023-05-24 11:28:09 +0300

    Added ColorModeSwitcher

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,11 @@ function App() {
       <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
-      <Show above ="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
+      <Show above="lg">
+        <GridItem area="aside">Aside</GridItem>
       </Show>
 
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
